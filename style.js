4a2b77b1e6ffe8eb738b4741218adf885559376a
SakriyaDangol <sakriyadangol003@gmail.com>
2021-06-01 17:09:22 +0545

    updated

---
+++ style.js
@@ -0,0 +1,22 @@
+const My_Input = document.createElement('input');
+document.body.appendChild(My_Input);
+
+const Btn = document.createElement('button');
+document.body.appendChild(Btn);
+Btn.innerHTML = 'Add'
+
+
+const UL = document.createElement('ul');
+document.body.appendChild(UL);
+
+Btn.addEventListener('click', showInput);
+
+
+function showInput(){
+
+    const Text = document.createTextNode(My_Input.value);
+    const LI = document.createElement('li');
+    UL.appendChild(LI);
+    LI.appendChild(Text);
+    document.querySelector('input').value = 'Enter Text';
+}
